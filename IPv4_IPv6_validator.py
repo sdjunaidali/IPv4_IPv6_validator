6c79edffd73cce3fb6b162d47454a1153ec3b106
Junaid <sd.junaidali@gmail.com>
2015-11-17 02:21:01 +0530

    Updated for abbreviated IPv6 addresses

--- IPv4_IPv6_validator.py
+++ IPv4_IPv6_validator.py
@@ -1,45 +1,44 @@
-#!/usr/bin/env python
+
+import ipaddress
+
+
 def  check_IP( s):
-    is_ipv4 = True
-    is_ipv6 = True
+   try:
+       
+     ipaddress.ip_address(addr)
+     is_ipv4 = True
+     is_ipv6 = True
     
-    #check if IPv4
-    numbers = s.split(".")
-    if len(numbers) != 4:
+     #check if IPv4
+     numbers = addr.split(".")
+     if len(numbers) != 4:
         is_ipv4 = False
-    if (is_ipv4):
+     if (is_ipv4):
         for x in numbers:
             if not x.isdigit():
                 is_ipv4 = False
             i = int(x)
             if i < 0 or i > 255:
                 is_ipv4 = False
-            
-    #check if IPv6
-    if not is_ipv4:
-        numbers = s.split(":")
-        if len(numbers) != 8:
-            is_ipv6 = False
-        for x in numbers:
-            #check if exadecimal 
-            try:
-                is_hex = int(x, 16)
-            except:
-                is_ipv6 = False
-                break;
-        #if here, every number was hexadecimal
-        
-    if is_ipv4:
-        print "IPv4"
-    elif is_ipv6:
-        print "IPv6"
-    else:
-        print "Neither"
 
-finished = False
-while (not finished):
-    addr = raw_input("Insert address to validate: ")
-    if addr == "end":
-        finished = True
-    else:
-        check_IP(addr)
+     if is_ipv4:
+        print ("The address {0} is IPv4".format(addr))      
+
+     #If not IPv4 then It is IPv6 else throw error.
+     if not is_ipv4:
+         print("The address {0} is IPv6".format(addr))
+         
+        
+   except ValueError as e:
+            print(e)
+
+if __name__ == '__main__':
+
+    
+    while True:
+      addr = input("Insert address to validate: ")
+    
+      check_IP(addr)
+
+
+    
